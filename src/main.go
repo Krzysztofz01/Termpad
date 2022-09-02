c53f496bf22d94f05ac3eaf05ff9612c2ba53188
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-09-02 22:47:49 +0200

    Fea: Project and debugger setup

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	// TODO: Implement argument parser
+	if len(os.Args) != 2 {
+		// TODO: Invalid arg count
+		os.Exit(1)
+	}
+
+	//targetFilePath := os.Args[1]
+
+}
