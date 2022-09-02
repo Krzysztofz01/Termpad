987bafb37df0055194ccfbae8c0d107c120052ca
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-09-02 22:48:37 +0200

    Fea: Line implementation and tests

---
+++ src/line.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+// Structure representing a single line of text in the editor
+type Line struct {
+	buffer []rune
+}
+
+// Line structure initialization funcation
+func (line *Line) Init(stringLine string) error {
+	line.buffer = make([]rune, len(stringLine))
+
+	for index, char := range stringLine {
+		line.buffer[index] = char
+	}
+
+	return nil
+}
+
+// Return the line (buffer) length
+func (line *Line) GetBufferLength() int {
+	return len(line.buffer)
+}
+
+// Return the line in string representation
+func (line *Line) GetBufferAsString() *string {
+	builder := strings.Builder{}
+
+	for _, char := range line.buffer {
+		builder.WriteRune(char)
+	}
+
+	lineString := builder.String()
+	return &lineString
+}
+
+// Return the line in rune slice representation
+func (line *Line) GetBufferAsSlice() []rune {
+	return line.buffer
+}
+
+// Insert a given rune at the position specified by the given cursor
+func (line *Line) InsertBufferCharacter(char rune, cursor *Cursor) error {
+	xOffset := cursor.GetOffsetX()
+	if xOffset < 0 {
+		return errors.New("line: invalid x (horizontal) negative offset requested to insert")
+	}
+
+	if xOffset > len(line.buffer) {
+		return errors.New("line: invalid x (horizontal) out of bound offset requested to insert")
+	}
+
+	if len(line.buffer) == xOffset {
+		line.buffer = append(line.buffer, char)
+		return nil
+	}
+
+	bufferHead := line.buffer[:xOffset+1]
+	bufferTail := line.buffer[xOffset:]
+
+	line.buffer = append(bufferHead, bufferTail...)
+	line.buffer[xOffset] = char
+
+	return nil
+}
+
+// Remove a rune at the position specified by the given cursor
+func (line *Line) RemoveBufferCharacter(cursor *Cursor) error {
+	xOffset := cursor.GetOffsetX()
+	if xOffset < 0 {
+		return errors.New("line: invalid x (horizontal) negative offset requested to remove")
+	}
+
+	if xOffset > len(line.buffer) {
+		return errors.New("line: invalid x (horizontal) out of bound offset requested to remove")
+	}
+
+	bufferHead := line.buffer[:xOffset]
+	bufferTail := line.buffer[xOffset+1:]
+
+	line.buffer = append(bufferHead, bufferTail...)
+
+	return nil
+}
+
+// Return the rune at the position specified by the given cursor
+func (line *Line) GetBufferCharacter(cursor *Cursor) (rune, error) {
+	xOffset := cursor.GetOffsetX()
+	if xOffset < 0 {
+		return 0, errors.New("line: invalid x (horizontal) negative offset requested to get")
+	}
+
+	if xOffset > len(line.buffer) {
+		return 0, errors.New("line: invalid x (horizontal) out of bound offset requested to get")
+	}
+
+	targetChar := line.buffer[xOffset]
+	return targetChar, nil
+}
---
+++ src/line_test.go
@@ -0,0 +1,205 @@
+package main
+
+import "testing"
+
+func TestLineShouldInitializeForNonEmptyString(t *testing.T) {
+	line := new(Line)
+
+	if err := line.Init("Valid string value"); err != nil {
+		t.Fail()
+	}
+}
+
+func TestLineShouldInitializeForEmptyString(t *testing.T) {
+	line := new(Line)
+
+	if err := line.Init(""); err != nil {
+		t.Fail()
+	}
+}
+
+func TestLineBufferShouldHaveCorrectLength(t *testing.T) {
+	line := new(Line)
+
+	input := "Valid string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	if line.GetBufferLength() != len(input) {
+		t.Fail()
+	}
+}
+
+func TestLineShouldProduceSameOutputAsString(t *testing.T) {
+	line := new(Line)
+
+	input := "Valid string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	if *line.GetBufferAsString() != input {
+		t.Fail()
+	}
+}
+
+func TestLineShouldProduceSameOutputAsRuneSlice(t *testing.T) {
+	line := new(Line)
+
+	input := "Valid string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	runeSlice := line.GetBufferAsSlice()
+
+	for index, char := range runeSlice {
+		if rune(input[index]) != char {
+			t.Fail()
+		}
+	}
+}
+
+func TestLineShouldInsertCharacterAtValidPosition(t *testing.T) {
+	line := new(Line)
+
+	input := "Vali string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	cursor := new(Cursor)
+	if err := cursor.Init(4, 0); err != nil {
+		t.Fail()
+	}
+
+	if err := line.InsertBufferCharacter('d', cursor); err != nil {
+		t.Fail()
+	}
+
+	if *line.GetBufferAsString() != "Valid string" {
+		t.Fail()
+	}
+}
+
+func TestLineShouldNotInsertCharacterAtInvalidPosition(t *testing.T) {
+	line := new(Line)
+
+	input := "Vali string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	cursor := new(Cursor)
+	if err := cursor.Init(90, 0); err != nil {
+		t.Fail()
+	}
+
+	if err := line.InsertBufferCharacter('d', cursor); err == nil {
+		t.Fail()
+	}
+
+	if *line.GetBufferAsString() != "Vali string" {
+		t.Fail()
+	}
+}
+
+func TestLineShouldRemoveCharacterAtValidPosition(t *testing.T) {
+	line := new(Line)
+
+	input := "Valid string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	cursor := new(Cursor)
+	if err := cursor.Init(4, 0); err != nil {
+		t.Fail()
+	}
+
+	if err := line.RemoveBufferCharacter(cursor); err != nil {
+		t.Fail()
+	}
+
+	if *line.GetBufferAsString() != "Vali string" {
+		t.Fail()
+	}
+}
+
+func TestLineShouldNotRemoveCharacterAtInvalidPosition(t *testing.T) {
+	line := new(Line)
+
+	input := "Vali string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	cursor := new(Cursor)
+	if err := cursor.Init(90, 0); err != nil {
+		t.Fail()
+	}
+
+	if err := line.RemoveBufferCharacter(cursor); err == nil {
+		t.Fail()
+	}
+
+	if *line.GetBufferAsString() != "Vali string" {
+		t.Fail()
+	}
+}
+
+func TestLineShouldReturCharacterAtValidPosition(t *testing.T) {
+	line := new(Line)
+
+	input := "Valid string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	cursor := new(Cursor)
+	if err := cursor.Init(4, 0); err != nil {
+		t.Fail()
+	}
+
+	char, err := line.GetBufferCharacter(cursor)
+	if err != nil {
+		t.Fail()
+	}
+
+	if char != 'd' {
+		t.Fail()
+	}
+}
+
+func TestLineShouldNotReturCharacterAtInvalidPosition(t *testing.T) {
+	line := new(Line)
+
+	input := "Valid string"
+
+	if err := line.Init(input); err != nil {
+		t.Fail()
+	}
+
+	cursor := new(Cursor)
+	if err := cursor.Init(90, 0); err != nil {
+		t.Fail()
+	}
+
+	char, err := line.GetBufferCharacter(cursor)
+	if err == nil {
+		t.Fail()
+	}
+
+	if char != 0 {
+		t.Fail()
+	}
+}
