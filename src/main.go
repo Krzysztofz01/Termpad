c28d0f26944246fe0430370ab73a122f59b84a3b
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-09-03 21:56:21 +0200

    Fea: Program strucures initialization tree

--- src/main.go
+++ src/main.go
@@ -5,12 +5,33 @@ import (
 )
 
 func main() {
-	// TODO: Implement argument parser
 	if len(os.Args) != 2 {
-		// TODO: Invalid arg count
+		// TODO: Panic - Invalid arguments notification
 		os.Exit(1)
+		return
 	}
 
-	//targetFilePath := os.Args[1]
+	targetFilePath := os.Args[1]
 
+	console, err := CreateConsole()
+	if err != nil {
+		// TODO: Panic - Failed to create console API instance notification
+		os.Exit(1)
+		return
+	}
+
+	editor := new(Editor)
+	if err := editor.Init(targetFilePath, console); err != nil {
+		// TODO: Panic - Failed to create editor instance notification
+		os.Exit(1)
+		return
+	}
+
+	if err := editor.Start(); err != nil {
+		// TODO: Handle errors while editing
+		os.Exit(1)
+		return
+	}
+
+	os.Exit(0)
 }
