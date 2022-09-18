1afa3e49beb76643a629767c06e5cfe0f5bc0080
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-09-18 20:22:38 +0200

    Enh: Program initialization error notifications

--- src/main.go
+++ src/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		// TODO: Panic - Invalid arguments notification
+		// TODO: Implement title screen
+		printErrorMessage(errors.New("args: invalid program arguments"))
 		os.Exit(1)
 		return
 	}
@@ -15,33 +18,46 @@ func main() {
 
 	config := new(Config)
 	if err := config.Init(); err != nil {
-		// TODO: Panic - Failed to access or create config file notification
+		printErrorMessage(err)
 		os.Exit(1)
 		return
 	}
 
 	console, err := CreateConsole()
 	if err != nil {
-		// TODO: Panic - Failed to create console API instance notification
+		printErrorMessage(err)
 		os.Exit(1)
 		return
 	}
 
 	editor := new(Editor)
 	if err := editor.Init(targetFilePath, console, config); err != nil {
-		// TODO: Panic - Failed to create editor instance notification
+		printErrorMessage(err)
 		console.Dispose()
 		os.Exit(1)
 		return
 	}
 
 	if err := editor.Start(); err != nil {
-		// TODO: Handle errors while editing
+		printErrorMessage(err)
 		console.Dispose()
 		os.Exit(1)
 		return
 	}
 
-	console.Dispose()
+	if err := console.Dispose(); err != nil {
+		printErrorMessage(err)
+		os.Exit(1)
+	}
+
 	os.Exit(0)
+}
+
+const (
+	redColorCode   = "\033[31m"
+	resetColorCode = "\033[0m"
+)
+
+func printErrorMessage(err error) {
+	fmt.Printf("%sThe program encountered a problem! [ %s ]%s\n", redColorCode, err, resetColorCode)
 }
